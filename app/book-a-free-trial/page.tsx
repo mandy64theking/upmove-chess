51386c7427162ded5ae7678dec5d11edcd173b4c
Mandar Bakshi <mandy64theking@gmail.com>
2025-03-14 05:37:31 +0000

    fixed the form issue

--- app/book-a-free-trial/page.tsx
+++ app/book-a-free-trial/page.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -12,128 +12,145 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react"; // Add this import
 
 const formSchema = z.object({
-    name: z.string().min(2).max(50),
-    email: z.string().email(),
-    message: z.string().min(10).max(500),
-    age: z.number().int().positive().min(5).max(100),
-    phone: z.string().regex(/^\d{10}$/),
-})
+  name: z.string().min(2).max(50),
+  email: z.string().email(),
+  message: z.string().min(10).max(500),
+  age: z.number().int().positive().min(3).max(100),
+  phone: z.string().regex(/^\d{10}$/),
+});
 
 import CustomNavBar from "../customNavBar";
 
 export default function Page() {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            name: "",
-            email: "",
-            message: "",
-            age: 0,
-            phone: "",
-        },
-    });
+  const [isLoading, setIsLoading] = useState(false); // Add this state
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        const queryParams = new URLSearchParams(values as any).toString();
-        const url = `https://z5pjr4limdtaxlsjhrprc5gvge0auzbg.lambda-url.us-east-2.on.aws/?${queryParams}`;
-        fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            cache: 'force-cache',
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            window.location.reload();
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-    }
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+      age: 0,
+      phone: "",
+    },
+  });
 
-    return (
-        <div>
-            <CustomNavBar />
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5 mt-[20vh] bg-muted mb-5 min-w-70 w-[70vw] sm:w-[30vw] m-auto border-2 rounded-3xl p-5">
-                    <FormField
-                        control={form.control}
-                        name="name"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Name</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Enter Your Name" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Email</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Enter Your Email" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="message"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Message</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Enter Your Message" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="age"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Age</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Enter Your Age" {...field} onChange={(e) => form.setValue("age", parseInt(e.target.value, 10))} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="phone"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Phone</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Enter Your Phone" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormDescription>
-                        We'll never share your details with anyone else.
-                    </FormDescription>
-                    <Button className="cursor-pointer" type="submit">Submit</Button>
-                </form>
-            </Form>
-        </div>
-    );
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true); // Set loading state to true
+    const queryParams = new URLSearchParams(values as any).toString();
+    const url = `https://z5pjr4limdtaxlsjhrprc5gvge0auzbg.lambda-url.us-east-2.on.aws/?${queryParams}`;
+    await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "force-cache",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        form.reset();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      })
+      .finally(() => {
+        setIsLoading(false); // Set loading state to false
+      });
+  }
+
+  return (
+    <div>
+      <CustomNavBar />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-5 mt-[20vh] bg-muted mb-5 min-w-70 w-[70vw] sm:w-[30vw] m-auto border-2 rounded-3xl p-5"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Your Name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Your Email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Message</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Your Message" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="age"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Age</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter Your Age"
+                    {...field}
+                    onChange={(e) =>
+                      form.setValue("age", parseInt(e.target.value, 10))
+                    }
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Your Phone" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormDescription>
+            We'll never share your details with anyone else.
+          </FormDescription>
+          <Button className="cursor-pointer" type="submit" disabled={isLoading}>
+            {isLoading ? "Submitting..." : "Submit"}
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
 import Footer from "./footer";
 
 export const metadata: Metadata = {
   title: "Upmove Chess Academy | Chess Mentorship and Training",
-  description: "Upmove Chess Academy is a leading chess institute offering personalized mentorship and fun, engaging training for all skill levels. Our expert coaches focus on strategic growth through structured lessons and real-game analysis, making learning both effective and enjoyable!",
+  description:
+    "Upmove Chess Academy is a leading chess institute offering personalized mentorship and fun, engaging training for all skill levels. Our expert coaches focus on strategic growth through structured lessons and real-game analysis, making learning both effective and enjoyable!",
 };
 
 export default function RootLayout({
@@ -19,7 +20,11 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="apple-touch-icon.png"
+        />
       </head>
       <body>
         <ThemeProvider
